dc4bd02b1f5924740c6792c0e1aeb3222c0a0ac3
Edj209 <edgarsjv@gmail.com>
2018-05-07 22:44:49 -0700

    added links to sections

--- src/app/resume-page/resume-page.component.ts
+++ src/app/resume-page/resume-page.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-resume-page',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResumePageComponent implements OnInit {
 
-  constructor() { }
+  private fragment: string;
+
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    // this.route.fragment.subscribe(fragment => {this.fragment = fragment; });
+    this.router.events.subscribe(s => {
+      if (s instanceof NavigationEnd) {
+        const tree = this.router.parseUrl(this.router.url);
+        if (tree.fragment) {
+          const element = document.querySelector('#' + tree.fragment);
+          if (element) { element.scrollIntoView(true); }
+        }
+      }
+    });
   }
+
+
+  onAnchorClick ( ) {
+    this.route.fragment.subscribe ( f => {
+      const element = document.querySelector ( '#' + f );
+      if (element) { element.scrollIntoView(true); }
+    });
+  }
+
+
 
 }
