58d2ee2144973ba90df3207308ff21b7dbe9a88f
Edj209 <edgarsjv@gmail.com>
2018-05-09 01:26:06 -0700

    scrolling is now functional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { ResumePageComponent } from './resume-page/resume-page.component';
+import { SmoothScrollDirective, SmoothScrollToDirective } from './ng2-smooth-scroll.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ResumePageComponent
+    ResumePageComponent,
+    SmoothScrollToDirective,
+    SmoothScrollDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ng2-smooth-scroll.directive.spec.ts
@@ -0,0 +1,8 @@
+import { Ng2SmoothScrollDirective } from './ng2-smooth-scroll.directive';
+
+describe('Ng2SmoothScrollDirective', () => {
+  it('should create an instance', () => {
+    const directive = new Ng2SmoothScrollDirective();
+    expect(directive).toBeTruthy();
+  });
+});
