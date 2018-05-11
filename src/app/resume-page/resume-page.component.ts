59d65d239500fadd8e6a7d4239ec5a09b1f71217
Edj209 <edgarsjv@gmail.com>
2018-05-10 18:58:09 -0700

    refactored resume page component, added new cursor to links

--- src/app/resume-page/resume-page.component.ts
+++ src/app/resume-page/resume-page.component.ts
@@ -11,30 +11,10 @@ import { Router, NavigationEnd } from '@angular/router';
 })
 export class ResumePageComponent implements OnInit {
 
-  private fragment: string;
-
   constructor(private route: ActivatedRoute, private router: Router) { }
 
-  ngOnInit() {
-    // this.route.fragment.subscribe(fragment => {this.fragment = fragment; });
-    this.router.events.subscribe(s => {
-      if (s instanceof NavigationEnd) {
-        const tree = this.router.parseUrl(this.router.url);
-        if (tree.fragment) {
-          const element = document.querySelector('#' + tree.fragment);
-          if (element) { element.scrollIntoView(true); }
-        }
-      }
-    });
-  }
+  ngOnInit() { }
 
-  // scrolls to section
-  onAnchorClick ( ) {
-    this.route.fragment.subscribe ( f => {
-      const element = document.querySelector ( '#' + f );
-      if (element) { element.scrollIntoView(true); }
-    });
-  }
 
 
 
