a90cc8eecbcdc17d312bee9af554c0aad9f83268
Edj209 <edgarsjv@gmail.com>
2018-05-16 17:47:19 -0700

    added functionality to display data

--- src/app/blog-page/blog-page.component.ts
+++ src/app/blog-page/blog-page.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { BlogService } from '../shared/blog/blog.service';
+
 @Component({
   selector: 'app-blog-page',
   templateUrl: './blog-page.component.html',
   styleUrls: ['./blog-page.component.css']
 })
 export class BlogPageComponent implements OnInit {
+  posts: Array<any>;
 
-  constructor() { }
+  constructor(private blogService: BlogService) { }
 
   ngOnInit() {
+    this.blogService.getAll().subscribe(data => {
+      this.posts = data;
+    });
   }
 
 }
