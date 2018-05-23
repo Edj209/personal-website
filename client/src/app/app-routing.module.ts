f80a3ace062ebeb00da049a51f9cbf3d35ea60de
Edj209 <edgarsjv@gmail.com>
2018-05-23 01:08:43 -0700

    created form for posting blog post

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { ResumePageComponent } from './resume-page/resume-page.component';
 import { BlogPageComponent } from './blog-page/blog-page.component';
+import { PostPageComponent } from './post-page/post-page.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/resume-page', pathMatch: 'full'},
   {path: 'resume-page', component: ResumePageComponent},
-  {path: 'blog-page', component: BlogPageComponent }
+  {path: 'blog-page', component: BlogPageComponent },
+  {path: 'post-page', component: PostPageComponent }
 ];
 
 @NgModule({
