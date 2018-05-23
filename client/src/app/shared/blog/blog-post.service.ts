1c7e552aef72fb9c3c146dc1e9fd12176fd591e9
Edj209 <edgarsjv@gmail.com>
2018-05-23 01:33:49 -0700

    added save to post service

--- client/src/app/shared/blog/blog-post.service.ts
+++ client/src/app/shared/blog/blog-post.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class BlogPostService {
 
-  constructor() { }
+  public API = '//localhost:8080';
+  public BLOG_API = this.API + '/blog';
+
+  constructor(private http: HttpClient) { }
+
+  save(post: any): Observable<any> {
+    let result: Observable<Object>;
+
+    result = this.http.post(this.BLOG_API, post);
+
+    return result;
+  }
 
 }
