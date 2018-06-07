c009b87ff24d545335cc98a96eac6a0104b8cc4f
Edj209 <edgarsjv@gmail.com>
2018-06-07 02:16:38 -0700

    improved post endpoint, view bug solved

--- server/src/main/java/blogBackend/BlogController.java
+++ server/src/main/java/blogBackend/BlogController.java
@@ -1,6 +1,8 @@
 package blogBackend;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,9 +22,10 @@ public class BlogController {
         return postsRepository.findAll();
     }
 
-    @PostMapping(path = "/post")
-    public void addPost(@RequestBody Posts post) {
+    @PostMapping(path = "/post", consumes = "application/json")
+    public ResponseEntity<Void> addPost(@RequestBody Posts post) {
         postsRepository.save(post);
+        return new ResponseEntity<Void>(HttpStatus.CREATED);
     }
 
 }
